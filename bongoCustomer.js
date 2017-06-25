b427f1d6307fa754b58f22dc6c54dd4416faa3ef
eXo5 <mike.yingling15@gmail.com>
2017-06-24 23:10:10 -0400

    Finally got a stock_quantity select to work -thanks russo

--- bongoCustomer.js
+++ bongoCustomer.js
@@ -4,10 +4,15 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "x",
     database: "bongo"
 });
-connection.connect(function(err) {
+var id;
+var productName;
+var updateValue;
+var purchaseQuant;
+
+connection.connect(function(err) {//initial DB  connection
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
 
@@ -15,10 +20,11 @@ connection.connect(function(err) {
 
     function(err, res) {
       if (err) throw err;
+          orderProduct(res);//THIS is what's defined below - I pass the res from the connection into it to get it going.
 
-      console.log(res);
-          order(res);
-});
+      //console.log(res);
+
+});//end init DB connect
 
 
 // order("SELECT * FROM PRODUCTS");
@@ -39,7 +45,8 @@ connection.connect(function(err) {
 });
 
 
-function order(res){
+function orderProduct(res){
+
  inquirer.prompt([
     {
       name:"item",
@@ -51,16 +58,44 @@ function order(res){
         }
         return choiceArr;
       },
-      message:"What would you like to buy?"
+      message:"Purchase Something?"
     }
   ]).then(function(answers){
-    var query = "SELECT * FROM products '" + answers.item + "'";
-    connection.query(query, function(err, res) {
+    //console.log(res.product_name, res.stock_quantity);
+    productName = answers.item;
+    connection.query("SELECT item_id FROM products WHERE product_name = ?", productName, function(err, res) {
+      console.log(res[0].item_id);
+      id = res[0].item_id;
     if (err) throw err;
-    console.log(res);
-    console.log("maybe")
-    });//end query
+    }); 
+    function x(){ connection.query("SELECT stock_quantity FROM products WHERE item_id = " + id +";", function(err, res){
+      purchaseQuant = parseFloat(res[0].stock_quantity);
+      console.log(purchaseQuant); 
+    });
+    }
+    setTimeout(x,25);//This will break at 2ms(yes I tested it), I gave 25ms for good measure.
+    inquirer.prompt([
+      {
+        name: "number",
+        type: "input",
+        message:"How many " + answers.item + "s would you like?"
+      }]).then(function(answers){
+       // connection.query("UPDATE products SET ? WHERE ?"), [{}]
+       console.log(purchaseQuant - parseFloat(answers.number));
+       connection.query("UPDATE stock_quantity FROM products WHERE item_id = " + id + ";",function(err,res){
+        if (err) throw err;
+        console.log(res);
+       });
+       // console.log()
+      
+//       connection.query("UPDATE products SET stock_quantity = " + updateValue + " WHERE = "+ id + ";", function(err, res){ connection.query("UPDATE products SET stock_quantity = " + updateValue + " WHERE = " + id + ";", function(err, res){
+//         if (err) throw err;
+// })
+//        });//end prompt for quantity
+      });
+
+    //end query
 
       });//end query
- 
+
 }
